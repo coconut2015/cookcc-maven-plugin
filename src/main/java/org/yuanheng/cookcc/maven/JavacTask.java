93f16c5638e7bbbad555f3d29d28ff54aebd8382
Heng Yuan <superduperhengyuan@gmail.com>
2018-03-16 23:30:25 -0700

    Add copyright/license headers

--- src/main/java/org/yuanheng/cookcc/maven/JavacTask.java
+++ src/main/java/org/yuanheng/cookcc/maven/JavacTask.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2017-2018 Heng Yuan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.yuanheng.cookcc.maven;
 
 import java.io.File;
@@ -9,6 +24,9 @@ import org.codehaus.plexus.compiler.Compiler;
 import org.codehaus.plexus.compiler.CompilerConfiguration;
 import org.codehaus.plexus.compiler.CompilerException;
 
+/**
+ * @author	Heng Yuan
+ */
 class JavacTask implements Task
 {
 	private String m_src;
@@ -37,6 +55,7 @@ class JavacTask implements Task
 					config.addCompilerCustomArgument ("-A" + option.opt + "=" + option.arg, null);
 			}
 			HashSet<File> set = new HashSet<File> ();
+			m_src.replace ('/', File.separatorChar);
 			log.info ("CookCC is processing " + m_src);
 			set.add (new File (m_src));
 			config.setSourceFiles (set);
